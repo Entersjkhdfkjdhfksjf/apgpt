dfd56ff936ef3828131c969b970d384f537e7b33
Somethong <121753192+Entersjkhdfkjdhfksjf@users.noreply.github.com>
2024-03-10 15:13:01 +0530

    Update apgpt.py
    added more training sessions

--- apgpt.py
+++ apgpt.py
@@ -5,7 +5,7 @@ from torch.nn import functional as F
 # hyperparameters
 batch_size = 16 # how many independent sequences will we process in parallel?
 block_size = 32 # what is the maximum context length for predictions?
-max_iters = 5000
+max_iters = 10000
 eval_interval = 100
 learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
